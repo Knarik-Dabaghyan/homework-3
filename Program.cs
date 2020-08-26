e16c3b209a6a61f27d02eb1322b1723879bf6ccc
Knarik-Dabaghyan <69726910+Knarik-Dabaghyan@users.noreply.github.com>
2020-08-26 20:54:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Threading;
+using System.Diagnostics;
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            SortArray s = new SortArray();
+            Stopwatch stopWatch = new Stopwatch();
+            
+            WriteLine("Please enter the size of an array that you want to sort:");
+            int n = int.Parse(ReadLine());         
+            int[] a = new int[n];
+          
+             for (int i=0; i < a.Length; i++)
+            {
+                a[i] = rd.Next(1, 100);
+                Write(a[i]+"\t");
+            }
+            WriteLine();
+          WriteLine("Select which algorithm you want to perform: \n 1. Insertion sort \n 2. Bubble sort \n 3. Quick sort \n 4. Heap sort \n ");
+            int b = int.Parse(ReadLine());
+            switch (b)
+            {
+                case 1:
+                    WriteLine("Insertion Sort");
+                    s.InsertionSort(a);
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        Write(a[i] + "\t");
+                    }
+                    WriteLine();
+                    stopWatch.Start();
+                    s.InsertionSort(a);
+                    stopWatch.Stop();
+                    WriteLine();
+                    ForegroundColor = ConsoleColor.Green;
+                    WriteLine("Time InsertionSor : {0}", stopWatch.Elapsed);
+                    ResetColor();
+                    WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+
+                    break;
+                case 2:
+                    WriteLine("Bubble Sort");
+                    s.bubbleSort(a);
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        Write(a[i] + "\t");
+                    }
+                    WriteLine();
+                    stopWatch.Start();
+                    s.bubbleSort(a);
+                    stopWatch.Stop();
+                    WriteLine();
+                    ForegroundColor = ConsoleColor.Green;
+                    WriteLine("Time bubbleSort: {0}", stopWatch.Elapsed);
+                    ResetColor();
+                    WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+
+                    break;
+                case 3:
+                    WriteLine("Quick Sort");
+
+                    s.quickSort(a, 0, a.Length-1);
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        Write(a[i] + "\t");
+                    }
+                    stopWatch.Start();
+                    s.bubbleSort(a);
+                    stopWatch.Stop();
+                    WriteLine();
+                    ForegroundColor = ConsoleColor.Green;
+                    WriteLine("Time quickSort : {0}", stopWatch.Elapsed);
+                    ResetColor();
+
+                    WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+                    break;
+                case 4:
+                    WriteLine("Heap Sort");
+
+                    s.HeapSort(a, a.Length, a.Length);
+                    
+                        for (int j = 0; j < a.Length; j++)
+                        {
+                            Write(a[j] + "\t");
+                        }
+                    stopWatch.Start();
+                    s.bubbleSort(a);
+                    stopWatch.Stop();
+                    WriteLine();
+                    ForegroundColor = ConsoleColor.Green;
+                    WriteLine("Time HeapSort : {0}", stopWatch.Elapsed);
+                    ResetColor();
+                    WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+
+                    break;
+
+                default:
+                    WriteLine("Error value");
+                    break;
+            }
+           
+        }
+
+
+
+    }
+    }
+
---
+++ SortArray.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class SortArray
+    {
+    
+
+       public void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+
+        }
+       public void bubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {                      
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+       public int partition(int[] arr, int low,
+                                   int high)
+        {
+            int pivot = arr[high];
+
+            
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                
+                if (arr[j] < pivot)
+                {
+                    i++;
+
+                 
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            
+            int temp1 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp1;
+
+            return i + 1;
+        }
+       public void quickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            { int pi = partition(arr, low, high);              
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+        }
+
+        public void sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+                HeapSort(arr, n, i);          
+            for (int i = n - 1; i > 0; i--)
+            {
+               
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                HeapSort(arr, i, 0);
+            }
+        }
+
+       
+       public void HeapSort(int[] arr, int n, int i)
+        {
+            int largest = i; 
+            int l = 2 * i + 1; 
+            int r = 2 * i + 2; 
+            
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+            if (r < n && arr[r] > arr[largest])
+                largest = r;          
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                HeapSort(arr, n, largest);
+            }
+        }
+
+
+
+
+
+    }
+}
